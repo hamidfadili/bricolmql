2099516b7179a480785621b37d33cbae96d98ba3
FADILI Hamid <hamid.fadili@usmba.ac.ma>
2020-03-01 19:36:16 +0100

    add new spring boot project

---
+++ src/main/java/mql/dominators/brico/BricoApplication.java
@@ -0,0 +1,13 @@
+package mql.dominators.brico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mql/dominators/brico/BricoApplicationTests.java
@@ -0,0 +1,13 @@
+package mql.dominators.brico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
